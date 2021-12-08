59a79243ce39e2a893cc56857766ebd1e5173d64
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-12-07 23:18:57 -0300

    Feito rotas e acessado a API

---
+++ labex/src/pages/CreateTripPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CreateTripPage = () => {
+    return(
+        <>
+        <h1>Formulário para o administrador criar uma nova viagem </h1>
+        </>
+    )
+}
---
+++ labex/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const HomePage = () => {
+    return(
+        <>
+        <h1>LABEX</h1>
+        <button>Ver Viagens</button>
+        <button>Átra Adm</button>
+        </>
+    )
+}
