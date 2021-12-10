68d49fc934809e1441e726a4b5d005de8d93dc51
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2021-12-09 21:05:43 -0300

    Feito ProtectedPage e UnProtectedPage

--- labex/src/pages/ListTripsPage.js
+++ labex/src/pages/ListTripsPage.js
@@ -2,35 +2,25 @@ import React from "react";
 import { useHistory } from 'react-router-dom';
 import useRequestData from "../hooks/useRequestData";
 import { goToHomePage, goToApplicationFormPage } from "../routes/goTo"
+import CardTrip from "../componentes/CardTrip";
 
 const ListTripsPage = () => {
     const history = useHistory()
     const [tripsData] = useRequestData("/trips",{})
 
-    const tripsList = tripsData.trips && tripsData.trips.map((tri) => {
-        return(
-            <>
-            <div>
-            <p><b>Nome:</b> {tri.name}</p>
-            <p><b>Descrição:</b> {tri.description}</p>
-            <p><b>Planeta:</b> {tri.planet}</p>
-            <p><b>Duração:</b> {tri.durationInDays}</p>
-            <p><b>Data:</b> {tri.date}</p> 
-            </div>
-            </>
-        )
+    const tripsList = tripsData.trips && tripsData.trips.map((t) => {
+        return<CardTrip key={t} trip={t}/>
     })
 
     return(
         <>
         <div> 
-            <h1>Para vermos todas as viagens</h1>
+            <h1>Lista de Viagens</h1>
             {tripsList && tripsList.length > 0 ? tripsList : <p>Carregando...</p>}{}
                 <button onClick={() => goToHomePage(history)}>Voltar</button>
                 <button onClick={() => goToApplicationFormPage(history)}>Inscrever-se</button>
+                
         </div>
-       
-
         </>
     )
 }
