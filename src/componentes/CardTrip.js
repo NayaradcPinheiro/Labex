f2e28f8a7a8675e234536f3dc3e99abc22e63696
NayaradcPinheiro <nayaradc.pinheiro@gmail.com>
2022-01-06 18:48:20 -0300

    Feito CardTrip

---
+++ src/componentes/CardTrip.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CardTrip = (props) => {
+    const {name, description, planet, durationInDays, date} = props.trip
+
+    return(
+        <div>
+            <p><b>Nome:</b> {name}</p>
+            <p><b>Descrição:</b> {description}</p>
+            <p><b>Planeta:</b> {planet}</p>
+            <p><b>Duração:</b> {durationInDays}</p>
+            <p><b>Data:</b> {date}</p>
+        </div>
+    )
+}
+export default CardTrip;
